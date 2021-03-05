4ebd0ee4685a14194f60ad589bda18655418a1b0
Swapnil <swapnil.vishu.7027@gmail.com>
2021-03-06 01:58:40 +0530

    initial commit

---
+++ src/App/App.js
@@ -0,0 +1,45 @@
+
+import React, { useState } from 'react';
+import Values from 'values.js';
+import './App.css';
+import SingleColor from '../Components/SingleColor';
+
+function App() {
+
+  const [color,setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#f15025').all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try{
+      let colors = new Values(color).all(10)
+      setList(colors);
+    } 
+    catch(error){
+      setError(true);
+      console.log(error);
+    }
+    }
+    
+  return (
+    <>
+      <section className="container">
+        <h2>Color Generator</h2>
+          <form onSubmit={handleSubmit}>
+            <input type='text' value={color} onChange={(e) => setColor(e.target.value)
+            } className={`${error ? 'error' : null }`} placeholder="#f150425"/>
+            <button className="btn" type='submit'>Submit</button>
+          </form>
+      </section>
+
+      <section className="colors">
+        {list.map((color, index) => {
+          return <SingleColor key={index} {...color} index={index} hexColor={color.hex}/>
+        })}
+      </section>
+    </>
+  );
+}
+
+export default App;
